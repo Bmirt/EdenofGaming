06d4f0206445ebd8c9ab3c2ea1be76e23f5ddbbd
unknown <oeskoko@gmail.com>
2019-03-15 19:39:39 +0400

    main page

---
+++ client/src/final project/assets/js/slider.js
@@ -0,0 +1,30 @@
+let slider = {
+    container:document.getElementById("slider"),
+    items: document.getElementsByClassName("main__top__slider__image"),
+    itemIndex: 0,
+    prevbtn:document.getElementById("prev"),
+    nextbtn:document.getElementById("next"),
+    nextslide: function(){
+        slider.items[slider.itemIndex].classList.remove("active");
+        if (slider.itemIndex==slider.items.length-1) {
+            slider.itemIndex=0;
+        }
+        else{
+            slider.itemIndex++;
+        }
+        slider.items[slider.itemIndex].classList.add("active");
+    },
+    prevslide: function(){
+        slider.items[slider.itemIndex].classList.remove("active");
+        if(slider.itemIndex==0){
+            slider.itemIndex = slider.items.length-1;
+        }
+        else{
+            slider.itemIndex--;
+        }
+        slider.items[slider.itemIndex].classList.add("active");    
+    }
+}
+slider.prevbtn.addEventListener("click",slider.prevslide);
+slider.nextbtn.addEventListener("click",slider.nextslide);
+
