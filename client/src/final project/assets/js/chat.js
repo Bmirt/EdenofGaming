d996df8676b067ebe0646339e6ac234448c67ec3
unknown <oeskoko@gmail.com>
2019-03-16 19:43:17 +0400

    little repare

--- client/src/final project/assets/js/chat.js
+++ client/src/final project/assets/js/chat.js
@@ -6,15 +6,17 @@ function left(){
     document.getElementById("btn").style.transform="rotate3d(0, 1, 0, 180deg)"
     document.getElementById("btn").style.right="285px";
     document.getElementById("awesome").style.color="#931f65";
-    
+    document.getElementById("headchat").style.backgroundColor="#931f65";
+
     push="button";
     }
     else if(push=="button"){
         document.getElementById('chat').style.right="-270px";
         document.getElementById("btn").style.transform="rotate3d(0,0,0,0)";
         document.getElementById("btn").style.right="260px";
-        document.getElementById("awesome").style.color="#1f272a";
-
+        document.getElementById("awesome").style.color="#292b2c";
+         document.getElementById("headchat").style.backgroundColor="#292b2c";
+        
         push="chat";
     }   
 }
