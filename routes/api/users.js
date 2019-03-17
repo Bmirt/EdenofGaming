25335f114aeed36231092c9489ca7edd86e23d42
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-03-17 17:28:27 +0400

    another small fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -105,8 +105,7 @@ router.post("/login", (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: "Bearer " + token,
-              payload
+              token: "Bearer " + token
             });
           }
         );
