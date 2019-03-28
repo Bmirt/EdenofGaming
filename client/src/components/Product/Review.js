91f2cebc312a813c28dcef4cb08c838eef644d2b
Tornike Gagadze <tgagadze@gmail.com>
2019-03-28 21:15:59 +0400

    Like/Dislike Review Works but need some changes

--- client/src/components/Product/Review.js
+++ client/src/components/Product/Review.js
@@ -117,6 +117,9 @@ export default class Review extends React.Component {
                     text={item.text}
                     likes={item.likes}
                     dislikes={item.dislikes}
+                    replays={item.comments}
+                    reviewID={item._id}
+                    productID={this.state.productID}
                   />
                 ))
               ) : (
