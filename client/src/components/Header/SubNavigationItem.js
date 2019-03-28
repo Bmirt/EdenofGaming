6a6f96dc380d9ff6449f436084763deaea0f8413
Tornike Gagadze <tgagadze@gmail.com>
2019-03-28 15:12:11 +0400

    Like Dislike | corrected link hovers

--- client/src/components/Header/SubNavigationItem.js
+++ client/src/components/Header/SubNavigationItem.js
@@ -1,12 +1,15 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 class SubNavigationItem extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Link to="#" className="header__bottom__navigation__list--item--link">
+        <Link
+          to="#"
+          className="header__bottom__navigation__list--item--link"
+          style={{ textDecoration: "none" }}
+        >
           {this.props.title}
         </Link>
       </React.Fragment>
