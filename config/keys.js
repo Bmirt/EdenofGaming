5042b281c482076fa16240037b6fb931344e2a32
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-03-29 16:30:09 +0400

    secret key change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://bmirt12345:bmirt12345@thecluster-bonxq.mongodb.net/test?retryWrites=true",
-  secretOrKey: "shhIt'sSecret"
+  secretOrKey: "It's A Big Secret Pls Tell No One"
 };
