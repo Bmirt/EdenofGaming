183fd3994c38b5ea311e7ae02aecda8594fb9ded
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-03-29 16:25:47 +0400

    profile validation refactoring

--- validation/profile.js
+++ validation/profile.js
@@ -4,8 +4,6 @@ module.exports = function validateLoginInput(data) {
   let errors = {};
 
   data.handle = !isEmpty(data.handle) ? data.handle : "";
-  data.status = !isEmpty(data.status) ? data.status : "";
-  data.skills = !isEmpty(data.skills) ? data.skills : "";
 
   if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
     errors.handle = "Handle needs to be between 2 and 4 characters";
@@ -28,18 +26,6 @@ module.exports = function validateLoginInput(data) {
   if (!isEmpty(data.youtube)) {
     if (!Validator.isURL(data.youtube)) {
       errors.youtube = "Not a valid URL";
-    }
-  }
-
-  if (!isEmpty(data.twitter)) {
-    if (!Validator.isURL(data.twitter)) {
-      errors.twitter = "Not a valid URL";
-    }
-  }
-
-  if (!isEmpty(data.instagram)) {
-    if (!Validator.isURL(data.instagram)) {
-      errors.instagram = "Not a valid URL";
     }
   }
 
