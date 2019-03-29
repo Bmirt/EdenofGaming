09af9d15244736b9508fb16e4be3504fd3d26995
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-03-29 16:40:37 +0400

    products like/dislikes

--- routes/api/products.js
+++ routes/api/products.js
@@ -105,4 +105,198 @@ router.delete("/:productId", (req, res, next) => {
     });
 });
 
+// @route  POST api/products/like/:id
+// @desc   Like product
+// @access Private
+
+router.post(
+  "/like/:product_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Product.findById(req.params.product_id)
+        .then(product => {
+          var ourReview;
+          for (var i = 0; i < product.reviews.length; i++) {
+            if (product.reviews[i].id === req.params.review_id) {
+              ourReview = product.reviews[i];
+              console.log(product.reviews[i]);
+            }
+          }
+          if (
+            ourReview.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this product" });
+          } else if (
+            ourReview.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length > 0
+          ) {
+            //Get remove index
+            const removeIndex = ourReview.dislikes
+              .map(item => item.user.toString())
+              .indexOf(req.user.id);
+
+            //Splice out of array
+            ourReview.dislikes.splice(removeIndex, 1);
+          }
+
+          //Add user id to likes array
+          ourReview.likes.unshift({ user: req.user.id });
+
+          product.save();
+          return res.status(404).json(ourReview);
+        })
+        .catch(err => {
+          console.log(err.message);
+          return res.status(404).json({ postnotfound: "review not found" });
+        });
+    });
+  }
+);
+
+// @route  POST api/products/unlike/:id
+// @desc   Like product
+// @access Private
+
+router.post(
+  "/unlike/:product_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Product.findById(req.params.product_id)
+        .then(product => {
+          var ourReview;
+          for (var i = 0; i < product.reviews.length; i++) {
+            if (product.reviews[i].id === req.params.review_id) {
+              ourReview = product.reviews[i];
+            }
+          }
+          if (
+            ourReview.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "You have not yet liked this review" });
+          }
+
+          //Get remove index
+          const removeIndex = ourReview.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out of array
+          ourReview.likes.splice(removeIndex, 1);
+
+          //Save
+          product.save();
+          return res.status(404).json(ourReview);
+        })
+        .catch(err =>
+          res.status(404).json({ postnotfound: "review not found" })
+        );
+    });
+  }
+);
+
+// @route  POST api/posts/dislike/:id
+// @desc   dislike product
+// @access Private
+
+router.post(
+  "/dislike/:product_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Product.findById(req.params.product_id)
+        .then(product => {
+          var ourReview;
+          for (var i = 0; i < product.reviews.length; i++) {
+            if (product.reviews[i].id === req.params.review_id) {
+              ourReview = product.reviews[i];
+            }
+          }
+          if (
+            ourReview.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadydisliked: "User already liked this review" });
+          } else if (
+            ourReview.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            //Get remove index
+            const removeIndex = ourReview.likes
+              .map(item => item.user.toString())
+              .indexOf(req.user.id);
+
+            //Splice out of array
+            ourReview.likes.splice(removeIndex, 1);
+          }
+
+          //Add user id to dislikes array
+          ourReview.dislikes.unshift({ user: req.user.id });
+
+          product.save();
+          return res.status(404).json(ourReview);
+        })
+        .catch(err => {
+          console.log(err.stack);
+          res.status(404).json({ postnotfound: "post not found" });
+        });
+    });
+  }
+);
+
+// @route  POST api/posts/undislike/:id
+// @desc   undislike product
+// @access Private
+
+router.post(
+  "/undislike/:product_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Product.findById(req.params.product_id)
+        .then(product => {
+          var ourReview;
+          for (var i = 0; i < product.reviews.length; i++) {
+            if (product.reviews[i].id === req.params.review_id) {
+              ourReview = product.reviews[i];
+            }
+          }
+          if (
+            ourReview.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notdisliked: "You have not yet disliked this review" });
+          }
+
+          //Get remove index
+          const removeIndex = ourReview.dislikes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out of array
+          ourReview.dislikes.splice(removeIndex, 1);
+
+          //Save
+          product.save();
+          return res.status(404).json(ourReview);
+        })
+        .catch(err => res.status(404).json({ postnotfound: "post not found" }));
+    });
+  }
+);
+
 module.exports = router;
