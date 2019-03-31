14620f1359940615a4abd05208b8f4b0c9b08160
unknown <oeskoko@gmail.com>
2019-04-01 01:38:37 +0400

    nav again and again

--- client/src/final project/assets/js/header.js
+++ client/src/final project/assets/js/header.js
@@ -3,15 +3,15 @@ document.getElementById("button").addEventListener("click",left);
 let eat="burger";
 function left(){
     if(eat==="burger"){ 
-    document.getElementById('nav').style.display="flex";
+    //document.getElementById('nav').style.display="flex";
     document.getElementById('a').style.transform="rotate(23deg)"
     document.getElementById('b').style.display="none"
     document.getElementById('c').style.transform="rotate(-23deg)"
-    document.getElementById("button").style.marginTop="10px";
+   
     eat="koko";
     }
     else if(eat==="koko"){
-        document.getElementById('nav').style.display="none";
+        //document.getElementById('nav').style.display="none";
         document.getElementById('a').style.transform="rotate(0)"
         document.getElementById('b').style.display="block"
         document.getElementById('c').style.transform="rotate(0)"
@@ -19,21 +19,15 @@ function left(){
         eat="burger";
     }  
 }
+// document.getElementById("button").addEventListener("click",left);
 
-
-
-
-
-// var hamburgerButton = document.getElementById("button");
-// var mobileNav = document.getElementById("nav");
-
-// function openMobile() {
-//     mobileNav.classList.add('open');
-// }
-// function closeMobile() {
-//     mobileNav.classList.remove('open');
-// }
-
-// hamburgerButton.addEventListener('click', openMobile);
-// mobileNav.addEventListener('click', closeMobile);
-
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#nav").toggle("400ms");
+    });
+    $(window).resize(function(){
+        if($(window).width()>800){
+             $('#nav').removeAttr('style');
+    }
+});
+});
