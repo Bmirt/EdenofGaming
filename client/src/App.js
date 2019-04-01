d9c813599e491980c9c0f066bdc497ddbc5d7480
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-04-02 02:04:01 +0400

    Individualprofile started

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/UserPages/Register";
+import IndividualProfile from "./components/UserPages/IndividualProfile";
 import Main from "./components/Main/Main";
 import UserProfile from "./components/UserPages/UserProfile";
 import Footer from "./components/Footer/Footer";
@@ -13,7 +14,7 @@ import ProductDetails from "./components/Product/ProductDetails";
 import Admin from "./components/UserPages/Admin";
 import Cart from "./components/Main/Cart";
 import GlobalState from "./context/GlobalState";
-import ProductsPage from './components/Product/ProductsPage'
+import ProductsPage from "./components/Product/ProductsPage";
 class App extends Component {
   render() {
     return (
@@ -23,11 +24,16 @@ class App extends Component {
             <Route path="*" component={Header} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
-             <Route exact path="/" component={Main} />
+            <Route exact path="/" component={Main} />
             <Route exact path="/about" component={Map} />
 
             <Route exact path="/product/:id" component={ProductDetails} />
             <Route exact path="/products/:id" component={ProductsPage} />
+            <Route
+              exact
+              path="/IndividualProfile"
+              component={IndividualProfile}
+            />
 
             <Route
               exact
@@ -43,11 +49,11 @@ class App extends Component {
               path="/admin"
               component={() => (
                 <ProtectedRoute>
-                  <Admin/>
+                  <Admin />
                 </ProtectedRoute>
               )}
             />
-            <Route exact path="/cart" component={() => <Cart />} /> 
+            <Route exact path="/cart" component={() => <Cart />} />
             <Route path="/" component={Footer} />
           </div>
         </Router>
---
+++ client/src/components/UserPages/IndividualProfile.js
@@ -0,0 +1,119 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import userContext from "../../context/user-context";
+
+class IndividualProfile extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+    errors: {}
+  };
+  static contextType = userContext;
+  onHandleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onHandleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("/api/users/register", {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        password2: this.state.password2
+      })
+      .then(res => {
+        this.context.message("You Registered Successfully");
+        this.props.history.replace("/login");
+      })
+      .catch(err => this.setState({ errors: err.response.data }));
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <React.Fragment>
+        <form action="" className="form">
+          <div className="form__wrapper  top">
+            <input
+              onChange={this.onHandleChange}
+              type="text"
+              name="name"
+              required
+              autoComplete="off"
+              className="form__wrapper__register"
+              placeholder="username"
+            />
+            {errors.name && (
+              <p className="form__wrapper__popup username">{errors.name}</p>
+            )}
+          </div>
+
+          <div className="form__wrapper">
+            <input
+              onChange={this.onHandleChange}
+              type="text"
+              name="email"
+              required
+              autoComplete="off"
+              className="form__wrapper__register "
+              placeholder="email"
+            />
+            {errors.email && (
+              <p className="form__wrapper__popup email">{errors.email}</p>
+            )}
+          </div>
+
+          <div className="form__wrapper">
+            <input
+              onChange={this.onHandleChange}
+              type="password"
+              name="password"
+              required
+              autoComplete="off"
+              className="form__wrapper__register "
+              placeholder="password"
+            />
+            {errors.password && (
+              <p className="form__wrapper__popup password">{errors.password}</p>
+            )}
+          </div>
+          <div className="form__wrapper">
+            <input
+              onChange={this.onHandleChange}
+              type="password"
+              name="password2"
+              required
+              autoComplete="off"
+              className="form__wrapper__register "
+              placeholder="repeat password"
+            />
+            {errors.password2 && (
+              <p className="form__wrapper__popup repeatpassword">
+                {errors.password2}
+              </p>
+            )}
+          </div>
+
+          <Link to="/login" className="form__reg">
+            Click here to log in
+          </Link>
+
+          <input
+            onClick={this.onHandleSubmit}
+            type="submit"
+            value="Register"
+            className="form__submit"
+          />
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default IndividualProfile;
