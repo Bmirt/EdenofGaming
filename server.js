6aab3ada99aac151f27c55b01a0aad52f58d1743
bmirt <mircxu1898@gmail.com>
2019-05-30 12:23:30 +0400

    hi

--- server.js
+++ server.js
@@ -58,4 +58,12 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
+const path = require("path");
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
