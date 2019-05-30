e9771822b232284a8efef834a25edc6c4ea1141d
bmirt <mircxu1898@gmail.com>
2019-05-30 12:42:33 +0400

    another small refactoring

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -4,36 +4,6 @@ import Auth from "../utils/AuthMethods";
 import axios from "axios";
 
 class Product extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     product: null,
-  //     isLoaded: false,
-  //     profile: null
-  //   };
-  // }
-
-  // componentWillMount() {
-  //   window.scroll(0, 0);
-  //   const jwt = Auth.getJWT();
-  //   var config = {
-  //     headers: {
-  //       Authorization: jwt
-  //     }
-  //   };
-
-  //   axios
-  //     .get("http://localhost:5000/api/profile", config)
-  //     .then(res => {
-  //       this.setState({
-  //         profile: res.data
-  //       });
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }
-
   render() {
     const { id, image, platform, price, name } = this.props;
     const link = `/product/${id}`;
